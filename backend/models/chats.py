603677f483bc92a77d6622c60150557d1f6bf639
tajakmal <tajwar.akmal@gmail.com>
2023-07-14 23:13:26 -0400

    updated chats

--- backend/models/chats.py
+++ backend/models/chats.py
@@ -21,3 +21,14 @@ class ChatQuestion(BaseModel):
     question: str
     temperature: float = 0.0
     max_tokens: int = 256
+
+class ChatMessage(BaseModel):
+    model: str = "gpt-4"
+    question: str
+    # A list of tuples where each tuple is (speaker, text)
+    history: List[Tuple[str, str]]
+    temperature: float = 0.0
+    max_tokens: int = 256
+    use_summarization: bool = False
+    chat_id: Optional[UUID] = None
+    chat_name: Optional[str] = None
